f2fac95f2b2f856e02c40f3d7cf1d16d020644dd
Junki Yun <jk6841@gmail.com>
2025-07-26 12:31:30 +0900

    Initial commit
    - spring boot 프로젝트 초기화
    - 로컬 테스트 환경 준비
      - compose.yaml
        - mysql

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.5.4"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "io.github.jk6841"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test")
+    runtimeOnly("io.asyncer:r2dbc-mysql")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/main/java/io/github/jk6841/kakaoenterprise/KakaoEnterpriseApplication.java
@@ -0,0 +1,13 @@
+package io.github.jk6841.kakaoenterprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaoEnterpriseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KakaoEnterpriseApplication.class, args);
+    }
+
+}
